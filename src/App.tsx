a2be85ff1cd6c8487a45795957cc588dcd6ffca0
rsk <rsk>
2025-09-19 13:26:01 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+
+function App() {
+
+  return (
+    <Box>
+      xMode
+    </Box>
+  )
+}
+
+export default App
---
+++ src/app/api.ts
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const API = createApi({
+  reducerPath: 'API',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'api/bigtows.api/',
+  }),
+  tagTypes: [''], 
+  endpoints: () => ({}), 
+});
