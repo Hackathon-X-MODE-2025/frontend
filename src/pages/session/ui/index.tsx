1ef34e54b8e5867e4b613e729fc0fa0c115644a8
rsk <rsk>
2025-09-25 22:32:44 +0300

    dev-add-code

--- src/pages/session/ui/index.tsx
+++ src/pages/session/ui/index.tsx
@@ -1,13 +1,16 @@
 import { Box } from "@mui/material"
-import { EtlChat } from "../../../widgets/etl-chat/ui"
-import { EtlChatText } from "../../../widgets/etl-chat-text/ui"
+import { SessionsChooseSource } from "../../../widgets/session-choose-source/ui"
+// import { EtlChat } from "../../../widgets/etl-chat/ui"
+// import { EtlChatText } from "../../../widgets/etl-chat-text/ui"
 
 
 export const Session = () => {
     return (
         <Box className='h-full relative'>
-            <EtlChatText />
-            <EtlChat />
+            {/* <EtlChatText />
+            <EtlChat /> */}
+            <SessionsChooseSource />
+
         </Box>
     )
 }
